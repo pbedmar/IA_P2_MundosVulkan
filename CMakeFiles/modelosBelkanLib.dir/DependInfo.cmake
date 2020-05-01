d46b423eea590658a4966161abf9d8f3d5206a22
Pedro Bedmar Lopez <correo.pedrobedmar@gmail.com>
2020-05-01 13:33:10 +0200

    Añado el programa ejecutable proporcionado por el profesor y los primeros cambios en el nivel 1

---
+++ CMakeFiles/modelosBelkanLib.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/aldeano/aldeano.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/aldeano.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/aldeano/brazo.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/brazo.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/aldeano/cabeza.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/cabeza.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/aldeano/cintura.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/cintura.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/aldeano/pierna.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/pierna.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/aldeano/torso.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/torso.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/arbol/arbol.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/arbol/arbol.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/item/item.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/item/item.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/jugador/jugador3d.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/jugador/jugador3d.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/jugador/quesito.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/jugador/quesito.cpp.o"
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/src/modelos/retrato.cpp" "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/retrato.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pedrobedmar/Dropbox/IA/PRACTICAS/practica2_linux/CMakeFiles/obj3DLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
